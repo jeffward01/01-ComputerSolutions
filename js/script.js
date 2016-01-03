41387af2b8ed98c1f2ac0f7c230e844df5ce6fd6
Jeff Ward <jeffwardcoding@gmail.com>
2016-01-03 12:15:17 -0800

    website routed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,44 @@
 var app = angular.module('computer',['ngRoute']).config(
     ['$routeProvider', function($routeProvider){
         $routeProvider.
+        
+        //Main Screen
             when('/main',{
             templateUrl: '../templates/main.html',
             controller: 'MainController'
-        });
+        }).
+        //About Screen
+            when('/about',{
+            templateUrl: '../templates/about.html',
+            controller: 'MainController'
+        }).
+        //Services Screen
+            when('/services',{
+            templateUrl: '../templates/services.html',
+            controller: 'ServicesController'
+        }).
+        //Contact Screen
+            when('/contact', {
+            templateUrl: '../templates/contact.html',
+            controller: 'ContactController'
+        }).
+        
+        otherwise({redirectTo:'/main'})
     }])
 
-    .controller('MainController', [function(){
-        console.log("This is the main controller");
-        
-        
-    }]);
+
+
+    //Main Controller
+    .controller('MainController', [ '$scope', function($scope){
+
+        }])
+
+    //Services Controller
+    .controller('MainController', [ '$scope', function($scope){
+
+        }])
+
+    //Contact Controller
+    .controller('MainController', [ '$scope', function($scope){
+
+        }]);
